269901d2eaf89dbbbc4d5c7a7dbacd3598841b74
Archie <3113773093@qq.com>
2021-08-02 21:58:17 +0800

    first commit

---
+++ src/main/java/cn/tedu/git/GitTest.java
@@ -0,0 +1,15 @@
+package cn.tedu.git;
+
+/**
+ * @Package cn.tedu.git
+ * @Author Archie
+ * @Date 2021/8/2
+ * @Time 20:55
+ * @Version V1.0
+ * @Project git-test
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
